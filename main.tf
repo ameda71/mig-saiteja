a8a2215878a4bebae1e41c20199db1c093ef6bf8
ameda71 <amedasaiteja75@gmail.com>
2025-05-14 13:10:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,14 @@ resource "google_compute_instance_template" "default" {
   machine_type = "e2-micro"
 
   disk {
-    boot  = true
-    auto_delete = true
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
+  boot       = true
+  auto_delete = true
+
+  initialize_params {
+    image = "debian-11"
   }
+}
+
 
   network_interface {
     network = "default"
